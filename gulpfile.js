68e7b5a6d46eb670bb71eb992be453cce683fb31
Fili Santillan <fili06604@gmail.com>
2016-06-14 13:25:47 -0500

    ✅ [add] gulp-minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
+    minify = require('gulp-minify'),
     path = {},
     stylusTasks = ['styles'];
 
 path.watch = {
-    stylus: ['./assets/stylus/**/*.styl']
+    stylus: ['./dev/stylus/**/*.styl']
 };
 
 gulp.task('watch', function () {
@@ -14,19 +15,33 @@ gulp.task('watch', function () {
 });
 
 gulp.task('styles', function () {
-  gulp
-    .src('./assets/stylus/main.styl')
-    .pipe(stylus({
-      compress: true
-    }))
-    .pipe(rename('styles.css'))
-    .pipe(gulp.dest('./assets/css/'));
+    gulp
+        .src('./dev/stylus/main.styl')
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest('./assets/css/'));
 });
 
-gulp.task('concat', function(){
+gulp.task('concat', function() {
     gulp
-        .src('assets/js-dev/**/*.js')
+        .src('./dev/js/**/**.js')
         .pipe(concat('index.js'))
-        .pipe(gulp.dest('assets/js/'));
+        .pipe(gulp.dest('./assets/js/'));
 
+});
+
+gulp.task('minify', function(){
+    gulp
+        .src('./assets/js/index.js')
+        .pipe(minify({
+            ext:{
+                src:'-debug.js',
+                min:'.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.combo.js', '-min.js']
+        }))
+        .pipe(gulp.dest('./assets/js/'))
 });
